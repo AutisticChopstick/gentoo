40b7d482a184ea2b996603c696f49dfd1922593b
AutisticChopstick <48421228+AutisticChopstick@users.noreply.github.com>
2021-01-10 21:06:08 -0500

    Add files via upload

---
+++ xrandrscript.bash
@@ -0,0 +1,2 @@
+#!/bin/bash
+xrandr --output DisplayPort-1 --primary --mode 1920x1080 --rate 144.00 --output DisplayPort-2 --mode 1920x1080 --rate 60.00 --left-of DisplayPort-1 --output HDMI-A-2 --mode 1920x1080 --rate 60.00 --right-of DisplayPort-1
